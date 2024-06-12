c156557d4d2537a38ab8529b83a298c92ec5d0e5
Casey Cotton <kc.cotton@windowslive.com>
2024-06-12 14:26:42 +1200

    Added an event listener to every div to change colour when hovered. Moved css styling to script.js for future dynamic styling

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const container = document.getElementById("container");
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 256; i++) {
     const div = document.createElement("div");
+    div.style. width = "37.5px";
+    div.style.padding = 0;
+    div.style.height = "37.5px";
+    div.style.margin = 0;
+    div.style.border = 0;
+    div.style.backgroundColor = "blue";
     div.textContent = `${i + 1}`;
-    container.appendChild(div);
+    div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "red";
+    })
 
+    container.appendChild(div);
     
 }
