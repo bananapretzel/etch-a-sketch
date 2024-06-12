cf4bde90261f53ecc09705af85294c3bf35afb5e
Casey Cotton <kc.cotton@windowslive.com>
2024-06-13 10:41:04 +1200

    Added functionality to the drop down box so that users can choose the side length of the square and the application modifies to show it.

--- script.js
+++ script.js
@@ -1,20 +1,66 @@
+'use strict';
+
 const container = document.getElementById("container");
-let perfectSquare = 16;
-let squareRoot = Math.sqrt(perfectSquare);
+let squareRoot = 16;
+let perfectSquare = 256;
 
-for (let i = 0; i < squareRoot; i++) {
-    const div = document.createElement("div");
-    div.style. width = "37.5px";
-    div.style.padding = 0;
-    div.style.height = "37.5px";
-    div.style.margin = 0;
-    div.style.border = 0;
-    div.style.backgroundColor = "blue";
-    div.textContent = `${i + 1}`;
-    div.addEventListener("mouseover", () => {
-        div.style.backgroundColor = "red";
-    })
+const pixelLength = parseInt(window.getComputedStyle(container).width);
+const dropDownBox = document.getElementById("selectedSquare");
+let squareSideLengthInPx = pixelLength/squareRoot;
 
-    container.appendChild(div);
-    
+function populateSelectField() {
+    for (let i = 2; i <= 100; i++) {
+        const option = document.createElement("option");
+        option.value = i;
+        option.textContent = i;
+        dropDownBox.appendChild(option);
+
+    }
 }
+
+function createSquares() {
+    for (let i = 0; i < perfectSquare; i++) {
+        const div = document.createElement("div");
+        //console.log(`${pixelLength/squareRoot}`);
+        div.style.width = `${squareSideLengthInPx}px`;
+        div.style.padding = 0;
+        div.style.height = `${squareSideLengthInPx}px`;
+        div.style.margin = 0;
+        div.style.border = 0;
+        div.style.backgroundColor = "blue";
+        div.style.outline = "1px solid black";
+        //div.textContent = `${i + 1}`;
+
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = "red";
+        })
+        //console.log(div.style.width);
+        //console.log(div.style.height);
+        container.appendChild(div);
+    }
+    dropDownBox.selectedIndex = squareRoot-2;
+}
+
+function destroySquares() {
+ while (container.firstElementChild) {
+    container.removeChild(container.firstElementChild);
+ }
+}
+
+let selectedSquare = document.getElementById("selectedSquare");
+selectedSquare.addEventListener("change", function () {
+    squareRoot = this.value;
+    perfectSquare = squareRoot ** 2;
+    squareSideLengthInPx = pixelLength/squareRoot;
+
+    
+    console.log("perfectSquare = " + perfectSquare);
+    console.log("square root = " + squareRoot);
+
+    destroySquares();
+    createSquares();
+
+})
+
+populateSelectField();
+createSquares();
