c23c173f8ea06fe83570f0833168294e27b3922a
Casey Cotton <kc.cotton@windowslive.com>
2024-06-13 14:11:54 +1200

    Added a dropdown box that will change the colour of boxes being hovered over to random hsl colours (vivid colours)

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ const container = document.getElementById("container");
 let squareRoot = 16;
 let perfectSquare = 256;
 let squareInPercent = 100/squareRoot;
-
 const pixelLength = parseInt(window.getComputedStyle(container).width);
 const dropDownBox = document.getElementById("selectedSquare");
 let squareSideLengthInPx = pixelLength/squareRoot;
+const selectedSquare = document.getElementById("selectedSquare");
+const colourChoice = document.getElementById("colour");
+let isColourful = false;
 
 function populateSelectField() {
     for (let i = 2; i <= 100; i++) {
@@ -19,20 +21,25 @@ function populateSelectField() {
     }
 }
 
-function createSquares() {
-    for (let i = 0; i < perfectSquare; i++) {
+function createSquares(numSquares, colourful) {
+    for (let i = 0; i < numSquares; i++) {
         const div = document.createElement("div");
 
         div.style.width = div.style.heigh = `${squareInPercent}%`;
         div.style.backgroundColor = "black";
 
         div.addEventListener("mouseover", () => {
-            div.style.backgroundColor = "floralwhite";
+            if (colourful) {
+                div.style.backgroundColor = `hsl(${Math.floor(Math.random() * 360)}, 100%, 50%)`;
+            } else {
+                div.style.backgroundColor = "floralwhite";
+            }
         })
         container.appendChild(div);
     }
     dropDownBox.selectedIndex = squareRoot-2;
 }
+
 
 function destroySquares() {
  while (container.firstElementChild) {
@@ -40,16 +47,32 @@ function destroySquares() {
  }
 }
 
-let selectedSquare = document.getElementById("selectedSquare");
+
 selectedSquare.addEventListener("change", function () {
     squareRoot = this.value;
     perfectSquare = squareRoot ** 2;
     squareSideLengthInPx = pixelLength/squareRoot;
     squareInPercent = 100/squareRoot;
     destroySquares();
-    createSquares();
+    createSquares(perfectSquare, isColourful);
+});
 
-})
+colourChoice.addEventListener("change", function() {
+    if (this.value === "colour") {
+        destroySquares();
+        isColourful = true;
+        createSquares(perfectSquare, isColourful);
+    } else if (this.value === "mono") {
+        destroySquares();
+        isColourful = false;
+        createSquares(perfectSquare, isColourful);
+    }
+}
+
+)
+
+
 
 populateSelectField();
-createSquares();
+createSquares(perfectSquare, isColourful);
+colourChoice.selectedIndex = 0;
