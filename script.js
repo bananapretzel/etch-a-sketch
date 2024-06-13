29166317ef5196de0e98e493fc185feeb8a68086
Casey Cotton <kc.cotton@windowslive.com>
2024-06-13 13:18:43 +1200

    Added some style so it doesn't look like ass.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const container = document.getElementById("container");
 let squareRoot = 16;
 let perfectSquare = 256;
+let squareInPercent = 100/squareRoot;
 
 const pixelLength = parseInt(window.getComputedStyle(container).width);
 const dropDownBox = document.getElementById("selectedSquare");
@@ -21,21 +22,13 @@ function populateSelectField() {
 function createSquares() {
     for (let i = 0; i < perfectSquare; i++) {
         const div = document.createElement("div");
-        //console.log(`${pixelLength/squareRoot}`);
-        div.style.width = `${squareSideLengthInPx}px`;
-        div.style.padding = 0;
-        div.style.height = `${squareSideLengthInPx}px`;
-        div.style.margin = 0;
-        div.style.border = 0;
-        div.style.backgroundColor = "blue";
-        div.style.outline = "1px solid black";
-        //div.textContent = `${i + 1}`;
+
+        div.style.width = div.style.heigh = `${squareInPercent}%`;
+        div.style.backgroundColor = "black";
 
         div.addEventListener("mouseover", () => {
-            div.style.backgroundColor = "red";
+            div.style.backgroundColor = "floralwhite";
         })
-        //console.log(div.style.width);
-        //console.log(div.style.height);
         container.appendChild(div);
     }
     dropDownBox.selectedIndex = squareRoot-2;
@@ -52,11 +45,7 @@ selectedSquare.addEventListener("change", function () {
     squareRoot = this.value;
     perfectSquare = squareRoot ** 2;
     squareSideLengthInPx = pixelLength/squareRoot;
-
-    
-    console.log("perfectSquare = " + perfectSquare);
-    console.log("square root = " + squareRoot);
-
+    squareInPercent = 100/squareRoot;
     destroySquares();
     createSquares();
 
